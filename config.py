ec3f2b21eb3aa0f1f5b9078b8c142d7c1410f9fa
Nick Girardo <nick@Nicks-MBP.fios-router.home>
2016-05-16 14:29:01 -0400

    Added dummy config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+#These are all dummy values, and need to be changed to run the application
+twilio = {
+    'account_sid' : 'twil_account_sid',
+    'auth_token' : 'twil_auth_token',
+    'app_sid' : 'twil_app_sid',
+}
